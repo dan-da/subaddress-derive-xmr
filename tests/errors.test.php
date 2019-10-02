95c970c32e6b91f95f4f26e05508441f059d0dd5
danda <danda@osc.co.cr>
2019-10-02 09:54:15 -0700

    fix required arg test and add tests for mutually exclusive args and key_args_together

--- tests/errors.test.php
+++ tests/errors.test.php
@@ -8,14 +8,41 @@ class errors extends tests_common {
     
     public function runtests() {
         $this->test_required_args();
+        $this->test_key_args_together();
+        $this->test_mutually_exclusive_args();
     }
     
     protected function test_required_args() {
-        $expect_str = '(--view-priv and --spend-pub) or --mnemonic or --gen-key must be specified.';
+        $expect_str = '(--view-priv and --spend-pub) or --mnemonic or --gen-key or --seed must be specified.';
         $expect_rc = 1;        
         
         // check xprv derivation results in correct addresses.
         $params = ['format' => 'list'];
-        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'missing --key or --nmenonic' );
+        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'missing required arg' );
+    }
+    
+    protected function test_key_args_together() {
+        $expect_str = '--view-priv and --spend-pub must be used together';
+        $expect_rc = 1;        
+        
+        $params = ['format' => 'list', 'view-priv' => 'foo'];
+        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'view-priv and spend-pub must be used together' );
     }    
+    
+    protected function test_mutually_exclusive_args() {
+        $expect_str = 'These flags are mutually exclusive: --mnemonic, --gen-key, --seed, and (--view-priv, --spend-pub)';
+        $expect_rc = 1;        
+        
+        $params = ['format' => 'list', 'gen-key' => null, 'view-priv' => 1, 'spend-pub' => 1];
+        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'mutually exclusive args' );
+        
+        $params = ['format' => 'list', 'gen-key' => null, 'mnemonic' => 1];
+        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'mutually exclusive args' );
+        
+        $params = ['format' => 'list', 'gen-key' => null, 'seed' => 1];
+        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'mutually exclusive args' );
+        
+        $params = ['format' => 'list', 'mnemonic' => 1, 'seed' => 1];
+        $this->exec_params_expect_error( $params, $expect_rc, $expect_str, 'mutually exclusive args' );
+    }
 }
